3bea7abd4226d28a397d438210a28de93a221805
mansityagi12 <mansityagi1708@gmail.com>
2023-10-02 17:17:49 +0530

    project

---
+++ script.js
@@ -0,0 +1,63 @@
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+
+document.addEventListener("mousemove", function(dets) {
+    crsr.style.left = dets.x + "px"
+    crsr.style.top = dets.y + "px"
+    blur.style.left = dets.x - 200 + "px"
+    blur.style.top = dets.y - 200 + "px"
+})
+
+
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    height: "90px",
+    duration: 0.5,
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        markers: true,
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 1
+    }
+
+})
+
+gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
+        markers: true,
+        start: "top -30%",
+        end: "top -70%",
+        scrub: 2
+    }
+})
+
+gsap.from("#collon1", {
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+        trigger: "#collon1",
+        scroller: "body",
+        markers: true,
+        start: "top 50%",
+        end: "top 47%",
+        scrub: 1
+    }
+})
+
+gsap.from("#collon2", {
+    y: 70,
+    x: 70,
+    scrollTrigger: {
+        trigger: "#collon1",
+        scroller: "body",
+        markers: true,
+        start: "top 50%",
+        end: "top 47%",
+        scrub: 1
+    }
+})
